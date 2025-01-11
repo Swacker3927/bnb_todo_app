806239039126ff4d24f54bde1bb6994d2dc23dc7
Taewoo Lee <sai6321@naver.com>
2025-01-11 22:37:40 +0900

    오타 수정

--- Socket/index.js
+++ Socket/index.js
@@ -3,24 +3,24 @@ require('dotenv').config();
 const httpServer = require("http").createServer();
 
 // DB
-const ConnectSequelize = require('./lib/ConnectSequelize')
+const ConnectSequelize = require('./lib/ConnectSequelize');
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DATABASE } = process.env;
 ConnectSequelize(__dirname + '/models', DB_HOST, DB_USER, DB_PASS, DB_DATABASE, DB_PORT, console.log).then(db => {
 	global.$DB = db;
-	
+
 	const io = require("socket.io")(httpServer, {
 		cors: {
 			origin: "http://todo.bnb.com:5080",
 		},
 	});
-	
+
 	global.$IO = io;
 	require('./chat');
-	
+
 	const PORT = process.env.PORT || 3000;
-	
+
 	httpServer.listen(PORT, () =>
-		console.log(`server listening at http://sockek.bnb.com:${PORT}`)
+		console.log(`server listening at http://socket.bnb.com:${PORT}`)
 	);
 
 })
